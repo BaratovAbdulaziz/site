42a10a740631ead3bc48800a2fecaa5a3002590a
BaratovAbdulaziz <abdulazizbaratov12@gmail.com>
2024-12-03 22:26:43 +0500

    qwetrt

---
+++ home page/learn more/script.js
@@ -0,0 +1,12 @@
+// Smooth Scroll for Anchor Links
+document.querySelectorAll('a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Example: You can add more JavaScript functionality if needed, like interactive elements or form validation in the future.
